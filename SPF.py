b709b3eed9d98854f471722823383561286926d5
enjrolas <alex@artiswrong.com>
2013-04-05 12:22:09 +0000

    added a bit of code to ensure that the soldering head is up and deactivated at the start of the program (aftegram (after a couple mishaps) and moved all functions that modify peripheral devices into hardware.py

--- SPF.py
+++ SPF.py
@@ -6,46 +6,18 @@ import sys
 import serial
 import time
 import re
-import RPi.GPIO as GPIO
 import sys, traceback
-from pizypwm import *
 from factory import Point, Panel
+from hardware import *
 import pickle
 import urllib, urllib2
 
-E_STOP=3
-BACKINGS_SOLDERER=15
-VACUUM_GENERATOR=7
-SOLDERING_STATION=8
-TEST_STATION=10
-TEST_LIGHTS=11
-TABBING_CUTTER=19
-SOLDERING_POWER=21
-SOLDERING_ENABLE=23
-PICK_HEAD=24
-
-GPIO.setmode(GPIO.BOARD)
-#Sensors
-GPIO.setup(E_STOP, GPIO.IN, pull_up_down=GPIO.PUD_UP) # E-stop
-GPIO.setup(BACKINGS_SOLDERER, GPIO.IN, pull_up_down=GPIO.PUD_UP) # backings solderer logic
-
-#Solenoids and switches
-GPIO.setup(VACUUM_GENERATOR, GPIO.OUT) # vacuum generator solenoid
-GPIO.setup(SOLDERING_STATION, GPIO.OUT) # soldering station solenoid
-GPIO.setup(TEST_STATION, GPIO.OUT) # test station solenoid
-GPIO.setup(TEST_LIGHTS, GPIO.OUT) # test station lights
-GPIO.setup(TABBING_CUTTER, GPIO.OUT) # tabbing cutter solenoid
-GPIO.setup(SOLDERING_POWER, GPIO.OUT) # soldering power
-GPIO.setup(SOLDERING_ENABLE, GPIO.OUT) # soldering enable
-GPIO.setup(PICK_HEAD, GPIO.OUT) # pick head solenoid
-
-#set up the soldering power PWM output
-solderingPower = PiZyPwm(100, SOLDERING_POWER, GPIO.BOARD)
-solderingPower.start(100)
 
 points=[] #  
 
-
+### safe the solderer, for safety's safe
+solderingStationOff()
+solderingStationUp()
 ser = None
 port = "/dev/ttyUSB0"
 for tries in range(1,5):
@@ -77,7 +49,7 @@ def getRemainingStroke():
             strokeEnd=float(row['stroke_end'])
             fullStroke=panelLength+strokeLead+strokeEnd
             if currentStroke>strokeLead:   #we're already into a stroke, we'll have to push out this panel before we get to the next one
-                return panelLength-currentStroke
+                return fullStroke-currentStroke
             else:
                 return 0
         except Exception as e:
@@ -194,8 +166,9 @@ def pointAction():
     """move to next point in list and do something there"""
     if len(points)>0:
         currentPoint=points.pop(0)
-        moveConveyor(currentPoint.remainingDistance)
-        executeCommand(currentPoint.code)
+        if currentPoint.pointType!="start" and currentPoint.pointType!="end":
+            moveConveyor(currentPoint.remainingDistance)
+            executeCommand(currentPoint.code)
 
 
 def pullCommands():
@@ -366,73 +339,13 @@ def flushReceiveBuffer():
             break
         print(ser.read(bytes_in_buffer))
 
-def solderingStationOn():
-    print("solderingStationOn()")
-    GPIO.output(SOLDERING_ENABLE, True)
-
-def solderingStationOff():
-    print("solderingStationOff()")
-    GPIO.output(SOLDERING_ENABLE, False)
-
-def setSolderingPower(power):
-    print "setting soldering power to %d" % power
-    solderingPower.changeDutyCycle(100-power)
-
-def tabbingCutterDown():
-    print("tabbingCutterUp()")
-    GPIO.output(TABBING_CUTTER, False)
-
-def tabbingCutterUp():
-    print("tabbingCutterDown()")
-    GPIO.output(TABBING_CUTTER, True)
-
-def pickHeadDown():
-    print("pickHeadUp()")
-    GPIO.output(PICK_HEAD, False)
-
-def pickHeadUp():
-    print("pickHeadDown()")
-    GPIO.output(PICK_HEAD, True)
-
-def vacuumGeneratorOn():
-    print("suctionSolenoidOn()")
-    GPIO.output(VACUUM_GENERATOR, True)
-
-def vacuumGeneratorOff():
-    print("suctionSolenoidOff()")
-    GPIO.output(VACUUM_GENERATOR, False)
-
-def solderingStationUp():
-    print("solderingStationUp()")
-    GPIO.output(SOLDERING_STATION, False)
-
-def solderingStationDown():
-    print("solderingStationDown()")
-    GPIO.output(SOLDERING_STATION, True)
-
-def testStationUp():
-    print("testStationUp()")
-    GPIO.output(TEST_STATION, False)
-
-def testStationDown():
-    print("testStationDown()")
-    GPIO.output(TEST_STATION, True)
-
-def lightsOn():
-    print("lightsOn()")
-    GPIO.output(TEST_LIGHTS, True)
-
-def lightsOff():
-    print("lightsOff()")
-    GPIO.output(TEST_LIGHTS, False)
-
 try:
     print("pulling startup script to init tinyG")
-#    startup()
+    startup()
     print("initialized and ready to go")
     while(True):
         pullCommands()
         time.sleep(1)
 
 except KeyboardInterrupt:
-    GPIO.cleanup()
+    hardwareCleanup()
